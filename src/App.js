cc9d3562744ea7a6a8f7b91e1d734ac3d1f6c321
quynam <quynamle123@gmail.com>
2020-05-12 12:06:01 +0700

    hoan thanh demo Boxcolor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox/index';
 
 function App() {
   return (
     <div className="App">
       <h1>wellcome to React Hooks</h1>
+      <ColorBox />
     </div>
   );
 }
