48d1df75b74a83b88f1c06663adaefd5fd275bf6
quynam <quynamle123@gmail.com>
2020-05-12 17:10:18 +0700

    xu ly todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import ColorBox from './components/ColorBox/index';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend! 😍 ' },
+    { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+    { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(x=>x.id === todo.id);
+    if(index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index,1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="App">
-      <h1>wellcome to React Hooks</h1>
-      <ColorBox />
+      <h1>React Hooks - todoList</h1>
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
